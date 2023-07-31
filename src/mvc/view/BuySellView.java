de27aaa092d85809d8999881a76f5c432f45c4f1
UU_jeong <80961726+oU-Ua@users.noreply.github.com>
2023-07-31 17:46:13 +0900

    update

---
+++ src/mvc/view/BuySellView.java
@@ -0,0 +1,54 @@
+package mvc.view;
+
+import java.util.Scanner;
+import mvc.dto.Stock;
+import mvc.controller.StockController;
+
+public class BuySellView {
+	Scanner sc= new Scanner(System.in);
+	StockController controller = new StockController();
+	public BuySellView() {
+		 System.out.println("매수/매도를 하시겠습니까? 1. 매수   2. 매도  3. 뒤로  ");
+         if(sc.nextInt()==1)
+        	 this.inputBuy();
+         else if(sc.nextInt()==2)
+        	 this.inputSell();
+         else if(sc.nextInt()==3) {
+        	 MenuView mv = new MenuView();
+        	 mv.printMenu();
+         }
+	}
+	/**
+	  모델번호에 해당하는 설명을 수정하기 위해 키보드 입력처리하는 메소드
+	*/
+	public void inputBuy(){
+      System.out.print("매수하려는 종목을 입력하세요. ");
+      String stockName = sc.nextLine();
+      
+
+		System.out.print("얼마나 매수하시겠습니까?");
+		int amountBuy = Integer.parseInt(sc.nextLine());
+      	controller.buy( new Stock(stockName , amountBuy) );
+      
+		
+
+	}
+	
+	
+	/**
+	  모델번호에 해당하는 설명을 수정하기 위해 키보드 입력처리하는 메소드
+	*/
+	public void inputSell(){
+      System.out.print("매도하려는 종목을 입력하세요. ");
+      String stockName = sc.nextLine();
+      
+
+		System.out.print("얼마나 매도하시겠습니까?");
+		int amountSell = Integer.parseInt(sc.nextLine());
+      	controller.sell( new Stock(stockName , amountSell) );
+      
+		
+
+	}
+
+}
--- src/mvc/view/MenuView.java
+++ src/mvc/view/MenuView.java
@@ -2,6 +2,7 @@ package mvc.view;
 
 import java.util.Scanner;
 
+
 import mvc.controller.StockController;
 import mvc.dto.Stock;
 
@@ -37,20 +38,22 @@ public class MenuView{
 		   switch(menu){
 			   case "1" : 
 				   controller.stockAll();
-                 System.out.println("매수/매도를 하시겠습니까? 1. 매수   2. 매도  3. 뒤로  ");
-                 if(sc.nextInt()==1)
-                	 this.inputBuy();
-                 else if(sc.nextInt()==2)
-                	 this.inputSell();
-                 else if(sc.nextInt()==3) //뭘 넣어야뒤로갈까
+				   BuySellView bv = new BuySellView();
+//                 System.out.println("매수/매도를 하시겠습니까? 1. 매수   2. 매도  3. 뒤로  ");
+//                 if(sc.nextInt()==1)
+//                	 this.inputBuy();
+//                 else if(sc.nextInt()==2)
+//                	 this.inputSell();
+//                 else if(sc.nextInt()==3) //뭘 넣어야뒤로갈까
                  break;
 			   case "2" :
 	                 controller.stockUser();
-	                 System.out.println("매수/매도를 하시겠습니까? 1. 매수   2. 매도  3. 뒤로  ");
-	                 if(sc.nextInt()==1)
-	                	 this.inputBuy();
-	                 else if(sc.nextInt()==2)
-	                	 this.inputSell();
+	                 
+//	                 System.out.println("매수/매도를 하시겠습니까? 1. 매수   2. 매도  3. 뒤로  ");
+//	                 if(sc.nextInt()==1)
+//	                	 this.inputBuy();
+//	                 else if(sc.nextInt()==2)
+//	                	 this.inputSell();
 			   break;
 			   case "3" : 
 	                 controller.finMarket();
@@ -111,38 +114,7 @@ public class MenuView{
 
 
 
-	/**
-	  모델번호에 해당하는 설명을 수정하기 위해 키보드 입력처리하는 메소드
-	*/
-	public void inputBuy(){
-        System.out.print("매수하려는 종목을 입력하세요. ");
-        String stockName = sc.nextLine();
-        
 
-		System.out.print("얼마나 매수하시겠습니까?");
-		int amountBuy = Integer.parseInt(sc.nextLine());
-        	controller.buy( new Stock(stockName , amountBuy) );
-        
-		
-
-	}
-	
-	
-	/**
-	  모델번호에 해당하는 설명을 수정하기 위해 키보드 입력처리하는 메소드
-	*/
-	public void inputSell(){
-        System.out.print("매도하려는 종목을 입력하세요. ");
-        String stockName = sc.nextLine();
-        
-
-		System.out.print("얼마나 매도하시겠습니까?");
-		int amountSell = Integer.parseInt(sc.nextLine());
-        	controller.sell( new Stock(stockName , amountSell) );
-        
-		
-
-	}
  
 
 }
