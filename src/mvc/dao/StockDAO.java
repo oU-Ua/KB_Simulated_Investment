b222f3cf15e6ad2b9317e1a4dfb07c406e1f3597
unknown <imuu30071126@gmail.com>
2023-08-03 15:55:51 +0900

    0803 fix Impl

--- src/mvc/dao/StockDAO.java
+++ src/mvc/dao/StockDAO.java
@@ -3,5 +3,10 @@ package mvc.dao;
 import java.sql.SQLException;
 import java.util.List;
 
+import mvc.dto.Stock;
+import mvc.dto.UserStock;
+
 public interface StockDAO {
+
+	int updateAvgPrice(UserStock us, Stock buyStock);
 }
